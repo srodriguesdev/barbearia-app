48ed88923fff8b12906b08a88ed2a6ae37e87066
Silas Rodrigues <sldocorte@gmail.com>
2026-08-17 12:43:22 -0300

    Cria model Barbeiro

--- agenda/models.py
+++ agenda/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Barbeiro(models.Model):
+    nome = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=20, blank=True)
+    especialidades = models.CharField(max_length=100, blank=True)
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
