156809bea046e32ea6109716cbc073b43181a894
Silas Rodrigues <sldocorte@gmail.com>
2026-08-17 12:54:30 -0300

    Cria model Servico

--- agenda/models.py
+++ agenda/models.py
@@ -9,3 +9,13 @@ class Barbeiro(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Servico(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True)
+    duracao = models.PositiveBigIntegerField()
+    preco = models.DecimalField(max_digits=8, decimal_places=2)
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
