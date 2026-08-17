988a64714286ef18e5f96ec1b937343859c85ceb
Silas Rodrigues <sldocorte@gmail.com>
2026-08-17 14:07:16 -0300

    Registra models no Django Admin

--- agenda/admin.py
+++ agenda/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Barbeiro, Servico, Agendamento
+
+admin.site.register(Barbeiro)
+admin.site.register(Servico)
+admin.site.register(Agendamento)
+
 
 # Register your models here.
