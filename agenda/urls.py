ef715e458eaf951a6d39fa59b8e69a8fbe42855f
Silas Rodrigues <sldocorte@gmail.com>
2026-08-17 14:42:51 -0300

    Adiciona cadastro de usuarios

--- agenda/urls.py
+++ agenda/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("cadastro/", views.cadastro, name='cadastro'),
 ]
--- agenda/views.py
+++ agenda/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
+    # Renderiza a página inicial da aplicação
     return render(request, "agenda/home.html")
+
+def cadastro(request):
+    # Se o formulário foi enviado, recebe os dados preenchidos pelo usuário
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        # Valida os dados e cria o usuário caso estejam corretos
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    else:
+        # Se o usuário apenas acessou a página, cria um formulário vazio
+        form = UserCreationForm()
+
+    # Envia o formulário para o template cadastro.html
+    return render(request, "agenda/cadastro.html", {"form": form})
