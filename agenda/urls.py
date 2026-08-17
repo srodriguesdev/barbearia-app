4507de5bce942352167145a7f6d3bf0284ea6d4c
Silas Rodrigues <sldocorte@gmail.com>
2026-08-17 15:12:52 -0300

    Adiciona cadastro login e logout de usuarios

--- agenda/urls.py
+++ agenda/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("cadastro/", views.cadastro, name='cadastro'),
+    path("login/", views.entrar, name="login"),
+    path("logout/", views.sair, name="logout"),
 ]
--- agenda/views.py
+++ agenda/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
 
 def home(request):
     # Renderiza a página inicial da aplicação
@@ -20,3 +22,26 @@ def cadastro(request):
 
     # Envia o formulário para o template cadastro.html
     return render(request, "agenda/cadastro.html", {"form": form})
+
+def entrar(request):
+    # Se o formulário foi enviado, tenta autenticar o usuário
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            usuario = form.get_user()
+            login(request, usuario)
+            return redirect("home")
+
+    else:
+        # Exibe o formulário de login vazio
+        form = AuthenticationForm()
+
+    return render(request, "agenda/login.html", {"form": form})
+
+def sair(request):
+    # Encerra a sessão do usuário
+    logout(request)
+
+    #Redireciona para a pagina inicial
+    return redirect("home")
